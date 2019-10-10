4e4d861014840f19841f0a4f18131029a69bd4e6
Eric Chiu <EricChiu920@gmail.com>
2019-10-10 12:04:32 -0400

    test out different ways of conditionally rendering something

--- 02-jsx-demo/index.jsx
+++ 02-jsx-demo/index.jsx
@@ -1,9 +1,26 @@
-class JSXDemo extends React.Component {
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
   render() {
+    const num = getNum();
+
     return (
-      React.createElement('h1', null, 'React createElement')
+      <div>
+        <h1>Your Number is: {num}</h1>
+        <p>{ num === 7 ? 'Congrats!' : 'Unlucky. :(' }</p>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<JSXDemo />, document.getElementById('root'));
+// class JSXDemo extends React.Component {
+//   render() {
+//     return (
+//       React.createElement('h1', null, 'React createElement')
+//     );
+//   }
+// }
+
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
